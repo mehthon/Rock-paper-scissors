ce1891ff351a7264f9719f0c9bebe451e2ab6b94
mehthon <68113376+mehthon@users.noreply.github.com>
2021-03-21 13:19:07 +0330

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,6 +1,6 @@
 import random
 
-def intro():
+def intro(): #quick introduction of our game
     print("Hi welcome to our fun game")
     print("This is a rock paper scissors game with computer")
     print("Choose a value between P R S")
@@ -11,10 +11,10 @@ def main_game():
     my_point = 0
     op_point = 0
     while True:
-        if my_point < 3 and op_point < 3:
+        if my_point < 3 and op_point < 3: #This lline shows who reach 3 faster will win the game
             our_choice = input("Enter a value: ")
             choices = ["R" , "P" , "S"]
-            opponent_move = random.choice(choices)
+            opponent_move = random.choice(choices) #Choosing a random value
             if our_choice == "S" or our_choice == "P" or our_choice == "R":
                 print(our_choice,"VS",opponent_move)
                 if our_choice == opponent_move:
@@ -33,7 +33,7 @@ def main_game():
             else:
                 print("Robot won!")
             break
-        print("Your score ->",my_point,"Computer score ->",op_point)
+        print("Your score ->",my_point,"Computer score ->",op_point) #Score board
 
 
 
